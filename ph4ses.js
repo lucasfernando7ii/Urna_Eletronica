4c579b730d4e6a62af1357909650b4b0f868166e
Lucas Fernando <lucas7ifernando@gmail.com>
2023-03-13 20:13:15 -0300

    First Commit

---
+++ ph4ses.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de TAL',
+                partido: 'ABC',
+                fotos: [
+                    {url: '84.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome:'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[ 
+                    {url: '84.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+},
+{
+    titulo:'PREFEITO',
+    numeros: 2,
+    candidatos: [
+        {
+            numero: '99',
+            nome: 'Ciclano',
+            partido: 'ABC',
+            vice: 'Cic',
+            fotos: [
+                {url: '85.jpg', legenda: 'Prefeito'},
+                {url: '85_2.jpg', legenda: 'Vice-prefeito', small:true}
+            ]
+        },
+        {
+            numero: '84',
+            nome:'Zulano',
+            partido: 'QWERTY',
+            vice:'Zul',
+            fotos: [
+                {url: '85.jpg', legenda: 'Prefeito'},
+                {url: '85_2.jpg', legenda: 'Vice-Prefeito', small:true}
+            ]
+        },
+    ]
+  }
+];
